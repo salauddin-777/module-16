051211b2a05acf2651730e0134cb7fe439acbd45
salauddin-777 <mdsalauddinmina777@gmail.com>
2025-08-02 16:49:27 +0600

    second commit

---
+++ object.js
@@ -0,0 +1,24 @@
+
+
+// Object of JS
+
+// Premitive
+const bottle = {
+    brand: 'apple',
+    price: 50,
+    color: 'white',
+    isClean: false
+}
+
+// Non Premitive
+const subject = {
+    name: 'biology',
+    teacher: 'rashed mam',
+    examDate: '30 Feb',
+    chapters: ['first', 'seconds', 'third'],
+    exams:{
+        name: 'final exam',
+        marks: 100
+    }
+
+}
